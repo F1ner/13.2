00f46a82cf809ffbcd9ac25f790d4fa403e14360
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-18 15:59:16 +0200

    13 2 3

---
+++ Ababiy&Siglov13.2(3)/Banknote.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._2_3_
+{
+    internal class Banknote
+    {
+        string name;
+        int nominal, amount;
+
+        public Banknote() { }
+
+        public Banknote(string name, int nominal, int amount) 
+        {
+            this.Name = name;
+            this.Nominal = nominal;
+            this.Amount = amount;
+        }
+        public string Name {
+            get { return (name != "") ? name : "Неизвестно"; }
+            set { name = value; }
+        }
+        public int Nominal {
+            get
+            {
+                nominal = value;
+            }
+            set
+            {
+                if (nominal > 0) Console.WriteLine(nominal);
+                else Console.WriteLine("Такого номинала не бывает");
+            }
+        }
+        public int Amount {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+    }
+}
---
+++ Ababiy&Siglov13.2(3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._2_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banknote banknote = new Banknote();
+            Banknote banknote1 = new Banknote();
+            Banknote banknote2 = new Banknote();
+
+
+            Console.ReadKey();
+        }
+    }
+}
