8be8171afa4916419724df341a36eff8fe0416ca
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-18 15:06:24 +0200

    13.2

---
+++ Ababiy&Siglov13.2(1)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._2_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string c = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            Edition edition= new Edition(a,c,b);
+            Edition edition1 = new Edition();
+            Edition edition2 = new Edition("pavel","chechov",1912);
+
+            
+            Console.WriteLine($"{edition.ToString()}");
+            edition.God();
+            edition.Interval();
+            Console.ReadKey();
+        }
+    }
+}
