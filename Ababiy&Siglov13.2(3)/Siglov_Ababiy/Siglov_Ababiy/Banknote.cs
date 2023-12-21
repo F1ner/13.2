3283ffe0a3ecbaf6934f1aad45ddc9a7900c5c33
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-21 14:43:55 +0300

    Add files via upload

---
+++ Ababiy&Siglov13.2(3)/Siglov_Ababiy/Siglov_Ababiy/Banknote.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siglov_Ababiy
+{
+    class Banknote
+    {
+        string name;
+        int nominal, amount;
+
+        public Banknote() { }
+
+        public Banknote(string name, int nominal, int amount)
+        {
+            this.Name = name;
+            this.Nominal = nominal;
+            this.Amount = amount;
+        }
+        public string Name
+        {
+            get { return (name != "") ? name : "Неизвестно"; }
+            set { name = value; }
+        }
+        public int Nominal
+        {
+            get
+            {
+                return nominal ;
+            }
+            set
+            {
+
+                if (value > 0)
+                {
+                    nominal=value;
+                }
+                else
+                {
+                    nominal = 0;
+                    Console.WriteLine("Такого номинала не бывает");
+                }
+            }
+        }
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    amount=value;
+                }
+                else
+                {
+                    amount = 0;
+                    Console.WriteLine("Количество равно нулю");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name-{name}\tNominal-{nominal}\tAmount-{amount}";
+        }
+        public void Init()
+        {
+            Console.WriteLine("Введите название");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите номинал");
+            Nominal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество");
+            Amount= int.Parse(Console.ReadLine());
+        }
+        public void Kvo()
+        {
+            Console.WriteLine("количество лет:");
+            int y = int.Parse(Console.ReadLine());
+            this.Amount = amount + y;
+            Console.WriteLine(Amount);
+        }
+        public void Prov()
+        {
+            Console.WriteLine("Введите стоимость");
+            int x = int.Parse(Console.ReadLine());
+            if (x > nominal * amount)
+            {
+                Console.WriteLine("Суммы не достаточно");
+            }
+            else
+            {
+                Console.WriteLine("Суммы достаточно");
+            }
+        }
+    }
+}
---
+++ Ababiy&Siglov13.2(3)/Siglov_Ababiy/Siglov_Ababiy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siglov_Ababiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = Console.ReadLine();
+            //string c = Console.ReadLine();
+            //int b = int.Parse(Console.ReadLine());
+            //Edition edition = new Edition(a, c, b);
+            //Edition edition1 = new Edition();
+            //Edition edition2 = new Edition("pavel", "chechov", 1912);
+
+
+            //Console.WriteLine($"{edition.ToString()}");
+            //edition.God();
+            //edition.Interval();
+            //Software software = new Software();
+            //software.Name = "Kaspersy";
+            //software.Owner = "Russia";
+            //software.Prise = true;
+            //Console.WriteLine($"{software.ToString()}\n");
+            //Software software1 = new Software();
+            //Software software2 = new Software();
+            string d = Console.ReadLine();
+            int f = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            Banknote banknote = new Banknote();
+            banknote.Init();
+            Banknote banknote1 = new Banknote();
+            banknote1.Init();
+            Banknote banknote2 = new Banknote(d,f,z);
+            Console.WriteLine($"{banknote.ToString()}\n{banknote1.ToString()}\n{banknote2.ToString()}");
+            banknote.Kvo();
+            Console.WriteLine($"{banknote.ToString()}");
+            banknote.Prov();
+            Console.ReadKey();
+        }
+    }
+}
